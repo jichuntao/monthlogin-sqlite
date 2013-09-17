0ccd604e66c582bb2c0117d6b26efb2ab342947d
jichuntao <jichuntao@hotmail.com>
2013-09-17 11:31:25 +0800

    first commit

---
+++ inData.js
@@ -0,0 +1,163 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: jichuntao
+ * Date: 13-9-17
+ * Time: 上午11:30
+ * To change this template use File | Settings | File Templates.
+ */
+var ndir = require('ndir');
+var fs = require('fs');
+var lineReader = require('line-reader');
+var util = require('util');
+
+var arguments = process.argv.splice(2);
+if (arguments.length < 1) {
+    console.log('arguments error!');
+    return;
+}
+
+var dir = '/Work/StromWorkSpace/alog-redis/logData/';
+var dateIndex = 0;
+var langs;
+var timeo = getTimeoffset();
+var redis = require("redis");
+var client = redis.createClient();
+var acc = 0;
+var sleepacc = 0;
+client.on('ready', function () {
+    dateIndex = 0;
+    start();
+});
+setInterval(debuginfo, 10000);
+//开始执行
+function start() {
+    if (dateIndex == arguments.length) {
+        console.log('Over:' + new Date().toString());
+    }
+    langs = fs.readdirSync(dir + debug_args + '/');
+    langs = ignoreArr(langs);
+    nextfile();
+}
+function debuginfo() {
+    console.log('men:' + util.inspect(process.memoryUsage()) + ' - qps:' + Math.round(acc / 10));
+    acc = 0;
+}
+function exec(strs, cb) {
+    try {
+        var data = strs.substr(strs.indexOf('{'));
+        var key = strs.substring(0, strs.indexOf('{') - 1);
+        var obj = JSON.parse(data);
+        var logtime = key.split('-')[0];
+        var lang = key.split('-')[2];
+        var uid = obj.uid;
+        var servertime = parseTime(obj.servertime * 1000, lang);
+        var clienttime = obj.time;
+        var action = obj.action;
+        var ldata = obj.data;
+        var mouse = obj.mouse;
+        if (!logtime || !lang || !uid || !servertime || !clienttime || !action) {
+            return 0;
+        }
+
+        var item = {
+            'uid': uid,
+            'action': action,
+            'servertime': servertime,
+            'clienttime': clienttime,
+            'lang': lang,
+            'logtime': logtime,
+            'data': ldata,
+            'mouse': mouse
+        };
+        client.hget(lang, uid, function (err, data) {
+            if (err) {
+                console.log(err);
+                cb();
+                return 0;
+            }
+            var allData = {};
+            if (data) {
+                allData = JSON.parse(data);
+            }
+            allData[logtime] = item;
+            client.hset(lang, uid, JSON.stringify(allData));
+            sleepacc++;
+            if (sleepacc > 999) {
+                sleepacc = 0;
+                setTimeout(cb, 100);
+            } else {
+                cb();
+            }
+        });
+
+    }
+    catch (err) {
+        console.log(err + " # " + strs);
+        cb();
+    }
+}
+
+
+//下个文件
+function nextfile() {
+    if (i == langs.length) {
+        console.log('Over');
+        process.exit();
+        return 0;
+    }
+    var path = dir + debug_args + '/' + langs[i] + '/' + debug_args + '_monthlogin_' + langs[i] + '.log';
+    i++;
+    console.log("Start read:" + path);
+    openfile(path);
+}
+
+//开始打开文件
+function openfile(filename) {
+    lineReader.eachLine(filename, function (line, last, cb) {
+        //console.log(line);
+        acc++;
+        exec(line, function () {
+            if (last) {
+                cb(false);
+                setTimeout(nextfile, 10000);
+                return;
+            }
+            cb();
+        });
+    });
+}
+
+//过滤数组
+function ignoreArr(arr) {
+    var ret = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == '.DS_Store') {
+            continue;
+        }
+        ret.push(arr[i]);
+    }
+    return ret;
+}
+
+//获取各语言的时间差
+function getTimeoffset() {
+    var ret = {};
+    ret.am = -14400;
+    ret.de = 7200;
+    ret.fr = 7200;
+    ret.nl = 7200;
+    ret.it = 7200;
+    ret.pl = 7200;
+    ret.br = -10800;
+    ret.th = 25200;
+    ret.tw = 28800;
+    return ret;
+}
+
+//转换服务器时间
+function parseTime(num, lang) {
+    num = num + (timeo[lang] * 1000);
+    var date = new Date(num);
+    var ret = date.getUTCFullYear() + "-" + (date.getUTCMonth() + 1) + "-" + date.getUTCDate() + " " + date.getUTCHours() + ":" + date.getUTCMinutes() + ":" + date.getUTCSeconds();
+    return ret;
+}
